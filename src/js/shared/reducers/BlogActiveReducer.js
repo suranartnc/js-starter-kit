c1a0f1131c762ab4c0f6c1502c5a035b010d54e6
Suranart Niamcome <suranartnc@gmail.com>
2016-01-26 19:12:11 +0700

    clear active blog when the new fetch has come

--- src/js/shared/reducers/BlogActiveReducer.js
+++ src/js/shared/reducers/BlogActiveReducer.js
@@ -1,7 +1,10 @@
 export default function(state = {}, action) {
+	console.log(`Action received: ${action.type}`);
 	switch(action.type) {
 		case 'GET_THE_POST':
 			return action.res.items[0];
+		case 'GET_THE_POST_REQUEST':
+			return {};
 		default:
 			return state;
 	}
