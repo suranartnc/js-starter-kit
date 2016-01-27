48c8b633c4c24e34403c7d5f510ec481d69d31f9
Suranart Niamcome <suranartnc@gmail.com>
2016-01-27 12:26:31 +0700

    apply webpack dev server

--- src/js/server.js
+++ src/js/server.js
@@ -4,6 +4,16 @@ import serverRendering from './serverRendering';
 const app = express();
 app.use(express.static('public'));
 
+// start a webpack-dev-server
+var webpack = require('webpack');
+var webpackConfig = require('../../webpack.config.js');
+var compiler = webpack(webpackConfig);
+app.use(require("webpack-dev-middleware")(compiler, {
+    noInfo: true, 
+    publicPath: webpackConfig.output.publicPath
+}));
+app.use(require("webpack-hot-middleware")(compiler));
+
 app.use(serverRendering);
 
 const PORT = process.env.PORT || 3000;
